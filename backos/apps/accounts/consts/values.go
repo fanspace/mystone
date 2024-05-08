b5dbbe482cf7a566a579a7d86003a3af63ee55bb
fanspace <fanspace@sina.com>
2024-05-08 10:10:11 +0800

    Delete backos directory

--- backos/apps/accounts/consts/values.go
+++
@@ -1,8 +0,0 @@
-package consts
-
-const (
-	PWD_SECRET_STRING_FORE = "z2kxMGptMzBiMXJtbTxubXA0P2tleTg"
-	PWD_SECRET_STRING_BACK = "NYTY4NGUwODgxMzMxNGE0YjYxZTM5ZY" // DefaultPageSize is
-	BAN_TYPE_USERNAME      = "userStat"
-	BAN_TYPE_IPADDRESS     = "ipStat"
-)
